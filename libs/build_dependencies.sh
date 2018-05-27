19ccb2b6a109b40d5aaea8c487ec9095aa65f7fc
Sergey Budnikov <kingston1853@gmail.com>
2018-05-27 21:07:26 +1000

    Update

--- libs/build_dependencies.sh
+++ libs/build_dependencies.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-RESTSDK_VERSION="v2.9.1"
+RESTSDK_VERSION="v2.10.0"
 DEFAULT_LIB_DIRECTORY_PATH="."
 
 libDir=${1:-$DEFAULT_LIB_DIRECTORY_PATH}
@@ -17,7 +17,7 @@ install_cpprestsdk(){
 	(cd $restsdkDir && git checkout tags/$RESTSDK_VERSION -b $RESTSDK_VERSION)
 	mkdir "$restsdkBuildDir"
 	if [[ "$OSTYPE" == "linux-gnu" ]]; then
-		export CXX=g++-4.9
+		export CXX=g++-5
 	fi
 	(cd "$restsdkBuildDir" && cmake ../Release -DCMAKE_BUILD_TYPE=Release -DBUILD_SHARED_LIBS=OFF -DBUILD_TESTS=OFF -DBUILD_SAMPLES=OFF)
 	(cd "$restsdkBuildDir" && make)
